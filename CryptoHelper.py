f9f5a3572b9ddc4010d3d0f3c0bfbc7c9912e4c5
mkhoiqi <mkhoirurrizqi08@gmail.com>
2021-12-13 12:15:14 +0700

    [FEAT] Restore program

--- CryptoHelper.py
+++ CryptoHelper.py
@@ -1,4 +1,3 @@
-import random
 # FUNCTION TO CONVERT TEXT OR LIST OF CHAR TO ASCII CODE
 def ascii_generator(text):
     ascii_set = []
@@ -156,7 +155,7 @@ def decryptingText(cipher_text, triggering_word):
 
 
 def encryptingFile(file, triggering_word):
-    plain_text = readFile('uploaded_file/raw/'+file.filename)
+    plain_text = readFile(file)
 
     super_key = key_generator(plain_text, triggering_word)
     plain_text_ascii = ascii_generator(plain_text)
@@ -173,13 +172,11 @@ def encryptingFile(file, triggering_word):
     cipher_list_reversed = sequenceReverseProcessing(cipher_list_unreversed, getBlockSize(plain_text))
     cipher_text_final = listToString(cipher_list_reversed, True)
     
-    n = random.randint(100000000,999999999)
-    writeFile('uploaded_file/processed/'+str(n)+"_"+file.filename, cipher_text_final, 'Success Generating Cipher File !')
-    return cipher_text_final
+    writeFile('cipher_text.txt', cipher_text_final, 'Success Generating Cipher File !')
 
 
 def decryptingFile(file, triggering_word):
-    cipher_text = readFile('uploaded_file/raw/'+file.filename)
+    cipher_text = readFile(file)
 
     super_key = key_generator(cipher_text, triggering_word)
     cipher_text_ascii = ascii_generator(cipher_text)
@@ -196,9 +193,6 @@ def decryptingFile(file, triggering_word):
         plain_char = (cipher_char - current_key) % 255
         plain_list.append(chr(plain_char))
 
-
-    n = random.randint(100000000,999999999)
-    writeFile('uploaded_file/processed/'+str(n)+"_"+file.filename, listToString(plain_list, False), 'Success Generating Plain File !')
-    return plain_list
+    writeFile('decrypted_cipher_text.txt', listToString(plain_list, False), 'Success Generating Plain File !')
 
 
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask.wrappers import Response
 from flask_restful import Resource, Api
 from flask_cors import CORS
-from CryptoHelper import decryptingFile, decryptingText, encryptingText, encryptingFile
+from CryptoHelper import decryptingText, encryptingText
 import os
 
 app = Flask(__name__)
@@ -48,57 +48,12 @@ class Cryptography(Resource):
                 return 'Hanya dapat melakukan enkripsi dan dekripsi'
         # END::Checker function
 
-        # BEGIN::Encryptor function
-        def fileEncryptor(file, key, option):
-            if option == 'modifikasi':
-                processed_text = encryptingFile(file, key)
-                return processed_text
-            else:
-                return 'Jenis algoritma tidak tersedia'
-        # END::Encryptor function
-
-        # BEGIN::Decryptor function
-        def fileDecryptor(file, key, option):
-            if option == 'modifikasi':
-                processed_text = decryptingFile(file, key)
-                return processed_text
-            else:
-                return 'Jenis algoritma tidak diketahui'
-        # END::Decryptor function
-
-        # BEGIN::Checker function
-        def fileChecker(file, key, option, type):
-            if type == 'decrypt':
-                processed_text = fileDecryptor(file, key, option)
-                return processed_text
-            elif type == 'encrypt':
-                processed_text = fileEncryptor(file, key, option)
-                return processed_text
-            else:
-                return 'Hanya dapat melakukan enkripsi dan dekripsi'
-        # END::Checker function
-
-
-        if 'file' not in request.files:
-            type = request.form['type']
-            option = request.form['option']
-            raw_text = request.form['raw_text']
-            key = request.form['key']
-            processed_text = checker(raw_text, key, option, type)
-        else:
-            file = request.files['file']
-            file.save(os.path.join('uploaded_file/raw', file.filename))
-            option = request.form['option']
-            type = request.form['type']
-            key = request.form['key']
-            processed_text = fileChecker(file, key, option, type)
-
-        return processed_text
         type = request.json['type']
         option = request.json['option']
         raw_text = request.json['raw_text']
         key = request.json['key']
         processed_text = checker(raw_text, key, option, type)
+        return processed_text
 
 
 
